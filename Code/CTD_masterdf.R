a3316828ca56b46bc82cc7f0db05552da887ea1c
OScott19 <o.scott19@imperial.ac.uk>
2021-08-20 15:33:38 +0100

    Adding in the updated CTD exploration file

--- Code/CTD_masterdf.R
+++ Code/CTD_masterdf.R
@@ -24,6 +24,8 @@ rm(list = ls())
 #getwd()
 # standard pathway 
 #pathway.to.linux <- "../../../oenon/AppData/Local/Packages/CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc/LocalState/rootfs/home/oslin/"
+pathway.to.linux <- "../../../oenon/AppData/Local/Packages/CanonicalGroupLimited.Ubuntu20.04onWindows_79rhkp1fndgsc/LocalState/rootfs/home/oslin/"
+
 
 #save(pathway.to.linux, file = "pathway.Rdata")
 load("pathway.Rdata")
@@ -31,7 +33,7 @@ load("pathway.Rdata")
 setwd(pathway.to.linux)
 
 # now set actually locally
-setwd("Chapter1/Code/")
+setwd("PhDCH1/Code/")
 
 
 # and load in my DF of all the relevant files
@@ -57,6 +59,7 @@ r.df <- all.df[all.df$relevant == T,]
 save(all.df, r.df, file = "../Data/BODC/MoreRelevantFiles.Rdata") # saved so I don't need to do this again
 load("../Data/BODC/MoreRelevantFiles.Rdata")
 
+
 #### 
 
 
@@ -64,8 +67,11 @@ load("../Data/BODC/MoreRelevantFiles.Rdata")
 # date, text to transform date, pressure (& description), conductivity
 # temperature, lat, long, cruise ID (to check for duplicates)
 
-master.df <- data.frame(I = NA, DATE = NA, JULIANINFO = NA, PRESS = NA, PRESSTXT = NA, CONDUCTIVITY = NA, 
-                       TEMPERATURE = NA, SALINITY = NA, LAT = NA, LONG = NA, CRUISEID = NA, MAXDEPTH = NA, FILE = NA)
+master.df <- data.frame(I = NA, DATE = NA, JULIANINFO = NA, PRESS = NA, 
+                       PRESSTXT = NA, CONDUCTIVITY = NA, 
+                       TEMPERATURE = NA, SALINITY = NA, LAT = NA, 
+                       LONG = NA, CRUISEID = NA, MAXDEPTH = NA, 
+                       DEPTH = NA, FILE = NA)
 
 holding.df <-  master.df
 
@@ -75,6 +81,8 @@ holding.df <-  master.df
 # RIGHT LETS LOAD STUFF UP 
 
 ## PACKAGES FIRST 
+install.packages(c("ncdf4", "raster", "rgdal", "ggplot2", "chron"))
+install.packages(c("rio", "readr", "data.table", "feather"))
 
 library(ncdf4) # package for netcdf manipulation
 library(raster) # package for raster manipulation
@@ -85,30 +93,60 @@ library(chron)
 
 path.to.data <- "../Data/BODC/AllCDTData/"
 
+counter <- 4
 
-for (i in 1:length(r.df$file)) {
+for (i in 2001:length(r.df$file)) {
   # to ensure we're not carrying over data, we resset the holding data frame to be empty each time
-  holding.df <- data.frame(DATE = NA, JULIANINFO = NA, PRESS = NA, PRESSTXT = NA, CONDUCTIVITY = NA, 
-                           TEMPERATURE = NA, SALINITY = NA, LAT = NA, LONG = NA, CRUISEID = NA, MAXDEPTH = NA)
+  holding.df <- data.frame(I = NA, DATE = NA, JULIANINFO = NA, PRESS = NA, 
+                           PRESSTXT = NA, CONDUCTIVITY = NA, 
+                           TEMPERATURE = NA, SALINITY = NA, LAT = NA, 
+                           LONG = NA, CRUISEID = NA, MAXDEPTH = NA, 
+                           DEPTH = NA, FILE = NA)
   
   # we then open the .nc file (one at a time)
   # w.data the "w." prefix here is "working" - ie will change every time
   w.data <- nc_open(file = paste(path.to.data, r.df$file[i], sep = ""))
-  # take a single variable (here, pressure)
-  w.pressure <- ncvar_get(w.data, "PRES")
-  # we now re-initialise the dataframe, this time using the single re-extracted variable
-  #  this makes the length of the df correct!
+  # store variables
   
-  holding.df <- data.frame(I = NA, DATE = NA, JULIANINFO = NA, PRESS = w.pressure, PRESSTXT = NA, CONDUCTIVITY = NA, 
-                           TEMPERATURE = NA, SALINITY = NA, LAT = NA, LONG = NA, CRUISEID = NA, MAXDEPTH = NA, FILE = NA)
+  # WORK OUT HOW THE DEPTH HAS BEEN STORED - AS PRESSURE OR AS DEPTH? ?
+  # if they have both - then use depth (more accurate?) but also record pressure
+
   
+  if(length(w.data$var$PRES) > 0) {
+    w.pressure <- ncvar_get(w.data, "PRES")
+    # we now re-initialise the dataframe, this time using the single re-extracted variable
+    #  this makes the length of the df correct!
+    holding.df <- data.frame(I = NA, DATE = NA, JULIANINFO = NA, PRESS = w.pressure, 
+                              PRESSTXT = NA, CONDUCTIVITY = NA, 
+                              TEMPERATURE = NA, SALINITY = NA, LAT = NA, 
+                              LONG = NA, CRUISEID = NA, MAXDEPTH = NA, 
+                              DEPTH = NA, FILE = NA)
+    holding.df$PRESSTXT <- w.data$var$PRES$units
+  }
+  
+  # however, it might be that it's stored directly as 'depth' (which is GREAT)
+   
+  if (length(w.data$var$DEPTH) > 0) {
+    w.depth <- ncvar_get(w.data, "DEPTH")
+    holding.df <- data.frame(I = NA, DATE = NA, JULIANINFO = NA, PRESS = NA, 
+                             PRESSTXT = NA, CONDUCTIVITY = NA, 
+                             TEMPERATURE = NA, SALINITY = NA, LAT = NA, 
+                             LONG = NA, CRUISEID = NA, MAXDEPTH = NA, 
+                             DEPTH = w.depth, FILE = NA)
+    
+  }
+  
+  if ((length(w.data$var$PRES) > 0) & (length(w.data$var$DEPTH) > 0)) {
+    holding.df$PRESS <- w.pressure
+    holding.df$PRESSTXT <- w.data$var$PRES$units
+  } 
+
   # and now we insert the rest of the data
   holding.df$I <- i
   holding.df$FILE <- r.df$file[i]
-    try(holding.df$DATE <- ncvar_get(w.data, "TIME"), silent = F, outFile = "Error - date")
+  try(holding.df$DATE <- ncvar_get(w.data, "TIME"), silent = F, outFile = "Error - date")
   # now put in the data that we will use to convert the Julian date
   try(holding.df$JULIANINFO <- w.data$var$TIME$units)
-  try(holding.df$PRESSTXT <- w.data$var$PRES$units)
   #holding.df$CONDUCTIVITY <- ncvar_get(w.data
   try(holding.df$LAT <- ncvar_get(w.data, "LATITUDE"))
   try(holding.df$LONG <- ncvar_get(w.data, "LONGITUDE"))
@@ -124,22 +162,75 @@ for (i in 1:length(r.df$file)) {
   
   
   if (i %% 100 == 0) {
-    print(i)  }
+    print(i)  
+    
+  
+  
+  if (object.size(master.df) >50000000 ) {
+    counter <- counter + 1
+    nametosave <- paste("../Data/LongMasterDF_", counter, (".Rdata"), sep = "")
+    save(master.df, file = nametosave)
+    
+    # then reset the master.df
+    
+    master.df <- data.frame(I = NA, DATE = NA, JULIANINFO = NA, PRESS = NA, 
+                            PRESSTXT = NA, CONDUCTIVITY = NA, 
+                            TEMPERATURE = NA, SALINITY = NA, LAT = NA, 
+                            LONG = NA, CRUISEID = NA, MAXDEPTH = NA, 
+                            DEPTH = NA, FILE = NA)
+    
+    }    
+    }
   
 }
 
-save(master.df, file = "../Data/CTD_masterDF.Rdata")
+rm(holding.df, r.df, w.data)
+
+test <- NA
+
+saveRDS(master.firsthalf, file = "C:/Users/oenon/CTD_master_larger.Rdata")
+save(master.df, file = "../Data/masterDF_larger")
+
+rm(master.df)
+master.firsthalf <- master.df[1:(length(master.df$I)/2),]
+master.secondhalf <- master.df[(length(master.df$I)/2):length(master.df$I),]
+
+object.size(master.secondhalf)
+
+length(master.df$DEPTH[master.df$DEPTH > 1000])
+max(master.df$DEPTH, na.rm = T)
 
 #load("../Data/CTD_masterDF.Rdata")
 
 length(unique(master.df$PRESS))
-length(unique(master.df$I)) # 422 unique statements???
-length(unique(r.df$file)) # especially when there are 2982 unique files. WHAT. 
+length(unique(master.df$I)) # now up to 2046 files!! increase of c.1500, but 900 to go
+length(unique(r.df$file)) # 2982 - great 
 length(unique(master.df$FILE)) # also only 422
 
-so clearly 
+# next step - find out why we can't load in those other 2500 files?
+
+missing <- r.df$file[r.df$file %in% master.df$FILE == F] #937 unentered files
+
+length(missing) + length(unique(master.df$FILE)) # yes we have the correct length
 
 
+# so, let's try and load in one of these missing files
+
+# stepping through the code in the loop
+i <- 423
+#this loads correctly: 
+w.data <- nc_open(file = paste(path.to.data, r.df$file[i], sep = ""))
+# however this doesn't: 
+w.pressure <- ncvar_get(w.data, "PRES") 
+# so the pressure isn't recorded as "PRES"
+print(w.data) #lets take a  look at the variables in the file
+
+w.data$var$DEPTH
+# so, the depth here is stored as Depth rather than pressure
+# so, we need to add an if statement to our loop
+
+length(w.data$var$DEPTH) > 0
+length(w.data$var$PRES) > 0
 
 
 
