c6a18ccfe3ab2d3f2d425a8af55d745fd90dab3d
OScott19 <os20359@essex.ac.uk>
2021-10-23 20:01:11 +0100

    Big day of working out kinks in retrieving CTD data, lots of progress!

--- Code/CTD_backgroundInfoAboutCTDs.R
+++ Code/CTD_backgroundInfoAboutCTDs.R
@@ -69,7 +69,8 @@ all.post1991 <- master.df[master.df$Year >= 1991,]
 
 ### plot distribution of points 
 
-ggplot(all.post1991, aes(x = Year)) + geom_histogram(binwidth = 1)  +
+library(ggplot2)
+ggplot(master.df, aes(x = YEAR)) + geom_histogram(binwidth = 1)  +
   theme_bw() + 
   labs(title = "CTD data points collected since 1991", x = "Year", y= "# CTD data points") +
   scale_y_continuous(limits = c(0,300000), breaks = seq(0,300000,50000)) + 
@@ -82,16 +83,27 @@ ggsave(filename = "../Figures/CTD_Since1991.png")
 
 ########## investigate how much data we have at different depths
 
-ggplot(all.post1991, aes(x = Year)) + geom_histogram(binwidth = 1)  +
+ggplot(master.df, aes(x = DEPTH)) + geom_histogram(binwidth = 1)  +
   theme_bw() + 
-  labs(title = "CTD data points collected since 1991", x = "Year", y= "# CTD data points") +
-  scale_y_continuous(limits = c(0,300000), breaks = seq(0,300000,50000)) + 
-  scale_x_continuous(limits =c(1991, 2018), breaks = seq(1991, 2019, 4))
+  labs(title = "CTD data points collected at different depths", x = "Depth", y= "# CTD data points") 
+  #scale_y_continuous(limits = c(0,300000), breaks = seq(0,300000,50000)) + 
+  #scale_x_continuous(limits =c(1991, 2018), breaks = seq(1991, 2019, 4))
 
 
 
+  
+ggsave(filename = "../Figures/CTD_accrossdepths.png")
 
-
+  
+########## investigate how much data we have from different seasons
+  
+ggplot(master.df, aes(x = MONTH)) + geom_histogram(binwidth = 0.5)  +
+    theme_bw() + 
+    labs(title = "CTD data points collected at different depths", x = "Month", y= "# CTD data points") +
+  scale_y_continuous(limits = c(0,800000), breaks = seq(0,800000,200000)) + 
+  scale_x_continuous(limits =c(0,12), breaks = seq(1, 12, 1))
+  
+ggsave(filename = "../Figures/CTD_accrossmonths.png")
 
 
 ### let's look at the nunber of expeditions that went out over time
--- Code/CTD_missingvariables_21Oct.R
+++ Code/CTD_missingvariables_21Oct.R
@@ -14,80 +14,37 @@ load("../Data/BODC/MoreRelevantFiles.Rdata") # generated in CTD*exploratory
 rm(all.df) # not necessary
 
 
+## load in most recent master df
+
+load("../Data/CTD_master_151023_convertedDepths.Rdata")
+path.to.data <- "../Data/BODC/AllCDTData/"
+
 ######################
 ### load in necessary packages
 
 library(ncdf4) # package for netcdf manipulation
-library(raster) # package for raster manipulation
-library(rgdal) # package for geospatial analysis
-library(ggplot2) # package for plotting
 library(chron) # converts julian dates (important!)
 
 
-#################
-
-# SEE LINE 179 FIRST
-
-######################
-# prime the counter and the dataframes
-path.to.data <- "../Data/BODC/AllCDTData/"
-
-load("../Data/CTD_master_151021_fixedCruiseID.Rdata")
-
-length(unique(master.df$FILE[is.na(master.df$MAXDEPTH)])) 
-# half of the records don't include bathymetric depth
-# that's weird
-# let's investigate
 
 ## JUST CHECKING WHERE THE NAS ARE
 length(master.df$I[is.na(master.df$I)]) # None!
 length(master.df$DATE[is.na(master.df$DATE)]) # also none
 length(master.df$DATE[is.na(master.df$DEPTH)]) # quite a few 
 length(master.df$JULIANINFO[is.na(master.df$JULIANINFO)])
-length(master.df$TEMPERATURE[is.na(master.df$TEMP_A)]) # quite a few - 1087995
-length(master.df$TEMPERATURE[is.na(master.df$TEMP_P)])
+length(master.df$TEMP_A[is.na(master.df$TEMP_A) == F]) # quite a few - 1087995
+length(master.df$TEMP_P[is.na(master.df$TEMP_P) == F])
 length(master.df$TEMPERATURE_UNIT[is.na(master.df$TEMPERATURE_UNIT)]) # more than before - weird! - 1413952
 length(master.df$LAT[is.na(master.df$LAT)]) # back to None! 
 length(master.df$LONG[is.na(master.df$LONG)]) # back to None phew 
 length(master.df$CRUISEID[is.na(master.df$CRUISEID)]) # back to None phew 
 length(master.df$FILE[is.na(master.df$FILE)]) # back to None phew 
-length(master.df$SALINITY[is.na(master.df$SALINITY)]) # 2709321 
+length(master.df$SAL_P[is.na(master.df$SAL_P) == F]) # 3022797 - only 0.5mil
+length(master.df$SAL_A[is.na(master.df$SAL_A) == F]) # 3204613 - and only 0.3mil
+### could the others have conductivity measurements instead?
 length(master.df$SALINITY_UNIT[is.na(master.df$SALINITY_UNIT)]) # 2708242
 ### so it turns out that we have quite a lot more missing temperature data than I thought
 # and we also have quite a lot of missing salinity data. not good!
-
-
-#### let's have a look at some of the files with missing temperature data, and see what's going on
-
-nc_close(w.data)
-
-no.temp <- master.df$FILE[is.na(master.df$TEMPERATURE) == T] # matches number above
-
-no.temp <- unique(no.temp) # 859 files - ok 
-
-w.data <- nc_open(file = paste("../Data/BODC/AllCDTData/", no.temp[1], sep = ""))
-# so we have data, under the TEMPST01 
-
-data <- ncvar_get(w.data, "TEMPST01") # ok it has values here
-
-data.inmaster <- master.df$TEMPERATURE[master.df$FILE == no.temp[1]] # the same??
-min(data.inmaster)
-min(data)
-# however it *contains* an NA somewhere
-temp.table <- as.data.frame(table(data))
-sum(temp.table$Freq) # contains a single NA somewhere
-data.wholeclip <- master.df[master.df$FILE == no.temp[1],] 
-# and it appears this NA is from meter 92, which inexplicably has no data in it. 
-
-
-#### TO CONTINUE 
-### WORK OUT WHY I HAVE SO MANY MISSING TEMPERATURES
-# REPLACE NA WITH SOMETHING AND THEN LOOP THROUGH EACH FILE
-# AND COUNT THE NUMBER OF INSTANCES OF NA
-# AND THEN LOOK INTO THOSE FILES
-
-# THEN REPEAT THIS WITH SALINITY INFORMATION
-# DECIDE IF ITS WORTH REMOVING ENTRIES WITHOUT TEMP & SALINITY DATA
 
 ####################################
 #####################################
@@ -97,7 +54,7 @@ data.wholeclip <- master.df[master.df$FILE == no.temp[1],]
 
 
 # let's only use the 
-practical.sals <- subset(master.df$SAL_P, is.na(master.df$SAL_P) == F)
+
 actual.sals <- subset(master.df$SAL_A, is.na(master.df$SAL_A) == F)
 
 master.df$LONG360 <- master.df$LONG + 360 # needed for marelac to run! 
@@ -106,6 +63,20 @@ master.df$LONG360 <- master.df$LONG + 360 # needed for marelac to run!
 ## try with a single value, and if that doesn't work, then re-think
 
 
+# CONVERTING FROM PRACTICAL TO ACTUAL
+practical.sals <- subset(master.df, is.na(master.df$SAL_P) == F)
+# also need pressure data
+
+
+practical.sals <- subset(practical.sals, is.na(practical.sals$PRESS) == F)
+#494k results
+
+p2as <- marelac::convert_PStoAS(S = practical.sals$SAL_P[1], p = practical.sals$PRESS[1]/10, 
+                                lat = practical.sals$LAT, 
+                                lon = practical.sals$LONG360, 
+                                Ocean = "Southern")
+??marelac::convert_PStoAS
+marelac::conver
 p2aS <-  marelac::convert_PStoAS(S = master.df$TEMP_P[subset(master.df$SAL_P, is.na(master.df$SAL_P) == F)], 
                                  #P = master.df$PRESS[master.df$SALINITY_UNIT == "Dmnless"], 
                                  lat = master.df$LAT[subset(master.df$SAL_P, is.na(master.df$SAL_P) == F)], 
@@ -211,94 +182,6 @@ install.packages("gsw")
 ## try this one
 
 gsw_pt_from_t
-
-
-
-##################################
-####################################
-##################################
-# SUCCESSFUL CODE ZONE!!
-##################################
-####################################
-##################################
-
-# EXPLORATORY CODE THAT WORKED- BEEN TIDIED AND MOVED INTO THE 'TIDY' SCRIPT
-
-
-
-# check how many rows are all NAs
-# get rid of rows that are ALL nas
-
-row.rm <- c()
-
-for (j in i:length(master.df$I)) {
-  row <- master.df[j,]
-  if (length(row[is.na(row)]) == 17) {
-    row.rm <- c(row.rm, j) }
-}
-
-
-
-# right - what else needs to be normalised?
-# Pressure needs to turn to depth!
-
-got.depth <- master.df$FILE[is.na(master.df$DEPTH) == F]
-
-# now lets open one of these ones
-
-w.data <- nc_open(file = paste(path.to.data, got.depth[2], sep= ""))
-
-print(w.data)
-
-# description: 
-# Depth (spatial coordinate) relative to water surface in the 
-#w ater body by profiling pressure sensor and conversion to seawater 
-# depth using UNESCO algorithm
-
-# WHICH UNESCO algorithm??
-# this one, I think: http://www.code10.info/index.php?option=com_content&view=article&id=67:calculating-the-depth-from-pressure&catid=54:cat_coding_algorithms_seawater&Itemid=79
-# find R package that can handle
-
-install.packages("marelac")
-
-pressures <- data.frame(PRES = master.df$PRESSS, UNIT = master.df$, SAL = master.df$SALINITY, 
-                        TEMP = master.df$TEMPERATURE, LAT = master.df$LAT)
-
-pressures <- pressures[is.na(master.df$PRESS) == F,] # get rid of pesks NAs
-
-one.pressure <- pressures[1,]
-one.depth <- marelac::sw_depth(P = (one.pressure$PRES), lat = one.pressure$LAT)
-# gives us a depth of -7. 
-# however if we make the pressure larger, then the dept goes positive, so not sure this is good
-# this doesn't really make sense, and also makes huge assumptions 
-# water of salinity 35, and temperature 0 dg C - both wrong!
-
-
-# let's try a different package
-
-library(oce)
-
-anothyer.depth <- oce::swDepth(pressure = (one.pressure$PRES), 
-                               latitude = one.pressure$LAT, eos = "unesco")
-# this gives us a more sensible depth of 3m (not a negative depth) - 
-
-oce.depths <- oce::swDepth(pressure = pressures$PRES, latitude = pressures$LAT, 
-                           eos = "unesco")
-
-# this doesn't 
-
-max(oce.depths) # reasonable!
-min(oce.depths) # zero. hmm. 
-
-
-# So, let's fill in the missing depths shall we?
-practice.vec <- oce::swDepth(pressure = master.df$PRESS[is.na(master.df$PRESS) == F ], latitude = master.df$LAT[is.na(master.df$PRESS) == F ], 
-                             eos = "unesco")
-save.depths <- master.df$DEPTH
-master.df$DEPTH[is.na(master.df$PRESS) == F ] <- oce::swDepth(pressure = master.df$PRESS[is.na(master.df$PRESS) == F ], latitude = master.df$LAT[is.na(master.df$PRESS) == F ], 
-                                                              eos = "unesco")
-
-checker <- data.frame(ORIG = save.depths, NEW = master.df$DEPTH) #yes they match
 
 
 
